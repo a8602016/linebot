bb14b9c7a42ab0a604212bc07a1c2efdafda446d
a8602016 <62063443+a8602016@users.noreply.github.com>
2023-08-07 11:52:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -32,8 +32,33 @@ def callback():
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
     #文字訊息格式
-    message = TextSendMessage(text = event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    # message = TextSendMessage(text = event.message.text)
+    # line_bot_api.reply_message(event.reply_token,message)
+
+    emoji = {
+        'index':0,
+        'productId':'5ac21184040ab15980c9b43a',
+        'emojiId':'255'
+    }
+    {
+        'index':17,
+        'productId':'5ac21184040ab15980c9b43a',
+        'emojiID':'225'
+    }
+
+    text_message = TextSendMessage(text = '''$ Master Finance $ Hello，您好，歡迎您成為Master
+                                    Finance的好友！
+                                   我是Master 財經小幫手
+                                   -這裡有股票，匯率資訊
+                                   -直接點選下方【圖中】選單功能
+                                   -期待您的光臨！''',emojis = emoji)
+    sticker_message = StickerSendMessage(
+        package_id = '8522',
+        sticker_id = '16581271'
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message,sticker_message])
 
 if __name__ == "__main__":
     app.run()
