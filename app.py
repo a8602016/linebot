aa9a5d809b11f6d77ef6676d52f3d8ea89485a78
a8602016 <62063443+a8602016@users.noreply.github.com>
2023-08-09 14:41:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -119,31 +119,30 @@ def handle_message(event):
 
 
 
-    if event.message.text =='查詢':
-        buttons_template = TemplateSendMessage(
-            alt_text='查詢',
-            template = ButtonsTemplate(
-            title = '選擇服務',
-            text = '請選擇',
-            thumbnail_image_url='https://i.imgur.com/DTX0I3q.jpg',
-            actions = [
-                MessageTemplateAction(
-                        label = '油價查詢',
-                        text = '@油價查詢'
-                ),
-                MessageTemplateAction(
-                    label = '匯率查詢',
-                    text = '@匯率查詢'
-                ),
-                MessageTemplateAction(
-                    label = '股價查詢',
-                    text = '@股價查詢'
+    if event.message.text=='查詢':
+            buttons_template=TemplateSendMessage(
+                alt_text='查詢',
+                template=ButtonsTemplate(
+                title='選擇服務',
+                text='請選擇',
+                thumbnail_image_url='https://i.imgur.com/DTX0I3q.jpg',
+                actions=[
+                    MessageTemplateAction(
+                label='油價查詢',
+                text='油價查詢'
+                    ),
+                    MessageTemplateAction(
+                label='匯率查詢',
+                text='匯率查詢'
+                    ),
+                    MessageTemplateAction(
+                label='股價查詢',
+                text='股票查詢'
+                    ),
+                ]
                 )
-            
-            ]
-        )
-    )
-        line_bot_api.reply_message(event.reply_token,buttons_template)
+            )
+            line_bot_api.reply_message(event.reply_token,buttons_template)
 
     
 
